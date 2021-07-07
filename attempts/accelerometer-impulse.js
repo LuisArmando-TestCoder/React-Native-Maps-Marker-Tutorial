139985648fa16c9571a8f37fd5f76421650cdaae
LuisArmando-TestCoder <luisarmando.murillobaltodano@gmail.com>
2021-07-06 19:21:03 -0600

    take impulse trash out

--- attempts/accelerometer-impulse.js
+++ attempts/accelerometer-impulse.js
@@ -1,19 +1,13 @@
 import React, {useState, useEffect} from 'react';
-import { Dimensions, StyleSheet, Text, View } from 'react-native';
-import MapView from 'react-native-maps'
-import * as Location from 'expo-location';
+import { StyleSheet, View } from 'react-native';
 import { Gyroscope, Accelerometer } from 'expo-sensors';
-const {width, height} = Dimensions.get('screen')
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  mapView: {
-    width,
-    height
   },
   circle: {
     width: 5,
@@ -25,31 +19,11 @@ const styles = StyleSheet.create({
 });
 
 export default function App() {
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
-  const [mapRegion, setMapRegion] = useState(null);
   const [gyroscope, setGyroscope] = useState(null);
   const [turns, setTurns] = useState({ y: 0 });
   const [accelerometer, setAccelerometer] = useState(null);
 
   useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setMapRegion({
-        ...location.coords,
-        longitudeDelta: 0.922,
-        latitudeDelta: 0.0421
-      });
-      setLocation(location);
-    })();
-
-    // 16 is fast
     Gyroscope.setUpdateInterval(100);
     Gyroscope.addListener(setGyroscope);
     Accelerometer.setUpdateInterval(100);
@@ -57,35 +31,24 @@ export default function App() {
   }, []);
 
   useEffect(() => {
-    console.log(gyroscope)
-    console.log(accelerometer)
-    const sensitivity = 20
+    const sensitivity = 20;
+
     setTurns({
       y: turns.y - (
         Math.sign(
           (accelerometer?.x * sensitivity) >> 0
         ) || 0
       )
-    })
+    });
   }, [gyroscope, accelerometer]);
 
   return (
     <View style={styles.container}>
-      <Text>hey</Text>
       <View style={{
         ...styles.circle,
         top: Math.sin(-turns?.y / 20) * 100,
         left: Math.cos(-turns?.y / 20) * 100
-      }}></View>
-
-      {/* <MapView
-        camera={{
-          heading: (gyroscope)
-        }}
-        rotateEnabled={true}
-        style={styles.mapView}
-        initialRegion={mapRegion}
-      ></MapView> */}
+      }}/>
     </View>
   );
 }
